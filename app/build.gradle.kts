99dd2a45ea57737eae4ffb84a545468487008df5
Juangomez05 <juanpgs1201@gmail.com>
2024-05-09 09:49:55 -0500

    Se termina primera version de la app frases de internet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,13 @@ dependencies {
     //activity
     implementation("androidx.activity:activity-ktx:$activity_version")
 
+    //Retrofit
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+
+    //Corrutinas
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.3.6")
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
